92b2bce9a2e941e18c076bbd07f779c8c29b1042
Jonatan <jona.jyn.27@hotmail.com>
2021-12-28 22:20:51 -0500

    Modificacion 2 jonatan

--- js/change.js
+++ js/change.js
@@ -1,34 +1,74 @@
-const form_3 = document.getElementById("form_change");
-const password_1 = document.getElementById("password");
-const password_2 = document.getElementById("password_repeat");
-const parrafo = document.getElementById("warnings")
-console.log("eeeee");
+const form_3 = document.getElementById('form_change');
+const inputs = document.querySelectorAll('#form_change input');
 
-form_3.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let warnings = "";
-    let entrar = false;
-    let regexPassword =
-        /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+const expresiones = {
+		password: /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/
 
-    if (password_1.value == password_2.value) {
-        if (!regexPassword.test(password_1.value)) {
-            warnings +=
-                "La contraseña debe de ser de 8 caracteres, tener al menos una Mayuscula, un numero y un carater epecial <br> ";
-            entrar = true;
-        }
-        if (regexPassword.test(password_1.value)) {
-            warnings += "<br> ";
-            entrar = true;
-            window.location = "login.html";
-        }
-    } else {
-        warnings +=
-        "(*) Las contraseñas deben ser iguales <br> ";
-        entrar = true;
+}
+
+const campos = {
+	 password: false,
+	
+}
+let bandera1 = false;
+let bandera2 = false;
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		
+		case "password1":
+			validarCampo(expresiones.password, e.target, 'password');
+			validarPassword2();
+		break;
+		case "password2":
+			validarPassword2();
+		break;
+		
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		const aviso=document.getElementById("aviso")
+        let a ="";
+        aviso.innerHTML=a;
+        bandera1=true;
+        		
+	} else {
+        const aviso=document.getElementById("aviso")
+        let a ="(*)La contraseña debe de ser de 8 caracteres, tener al menos una Mayuscula, un numero y un carater epecial <br> ";
+        aviso.innerHTML=a;
+	}
+}
+
+const validarPassword2 = () => {
+	const inputPassword1 = document.getElementById('password');
+	const inputPassword2 = document.getElementById('password_repeat');
+
+	if(inputPassword1.value !== inputPassword2.value){
+		const warnings =document.getElementById("warnings")
+        let a ="La contraseña debe de ser iguales <br> ";
+        warnings.innerHTML=a;
+
+	} else {
+        const warnings=document.getElementById("warnings")
+        let a ="";
+        warnings.innerHTML=a;
+        bandera2=true
+		
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+form_3.addEventListener('submit', (e) => {
+	e.preventDefault();
+    if(bandera2 && bandera1==true){
+        window.location = "login.html";
+        
     }
-    if(entrar){
-        parrafo.innerHTML=warnings
-    }
-
+    
 });
